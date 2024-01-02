6d61ea9ec074feb722aa33054ba7c0766e4d6d4a
Michael <94207078+michaelpeluso@users.noreply.github.com>
2024-01-02 05:02:19 +0000

    Update 7 files

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -12,6 +12,10 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
+        // solution to bug 2
+        onClick={() => {
+          onChange(!checked)
+        }}
       />
       <input
         id={inputId}
@@ -19,7 +23,7 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         className="RampInputCheckbox--input"
         checked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        // onChange={() => onChange(!checked)}
       />
     </div>
   )
--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -71,7 +71,7 @@ export function InputSelect<TItem>({
                 "RampInputSelect--dropdown-container-opened": isOpen,
               })}
               {...getMenuProps()}
-              style={{ top: dropdownPosition.top, left: dropdownPosition.left }}
+              style={{ top: dropdownPosition.top, left: dropdownPosition.left, position: "absolute" }} // solution to bug 1
             >
               {renderItems()}
             </div>
