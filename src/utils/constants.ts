c9dd5cc7851f0da88ecd19c63fe5faae0b7ee012
Michael <94207078+michaelpeluso@users.noreply.github.com>
2024-01-11 22:43:43 +0000

    Update 3 files

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { Employee } from "./types"
 
 export const EMPTY_EMPLOYEE: Employee = {
-  id: "all--employees", //unique id for all employees
+  id: "",
   firstName: "All",
   lastName: "Employees",
 }
